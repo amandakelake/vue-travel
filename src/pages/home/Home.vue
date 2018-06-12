36f18c76dca1c0aa7ace9cafdcea3f981498c0bc
罗广聪 <930007017@qq.com>
2018-06-12 23:20:46 +0800

    完成项目初始化

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>home</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style>
+  .home {
+    font-size: 50px;
+  }
+</style>
